52b24cc7810d73a8e98f410694ee9a502998491b
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-03-31 21:39:27 -0300

    Classe tabuleiro console

--- src/br/com/jopaulo/campominado/Aplicacao.java
+++ src/br/com/jopaulo/campominado/Aplicacao.java
@@ -1,18 +1,14 @@
 package br.com.jopaulo.campominado;
 
 import br.com.jopaulo.campominado.modelo.Tabuleiro;
+import br.com.jopaulo.campominado.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
-	}
 
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+
+	}
 }
