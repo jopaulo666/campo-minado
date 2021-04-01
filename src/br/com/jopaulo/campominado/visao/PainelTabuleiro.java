8c6f53883146c143b923e474b32b645265825c62
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-04-01 12:00:30 -0300

    Implementando a interface gráfica

---
+++ src/br/com/jopaulo/campominado/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo.campominado.visao;
+
+import javax.swing.JPanel;
+
+import br.com.jopaulo.campominado.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		// TODO Auto-generated constructor stub
+	}
+}
